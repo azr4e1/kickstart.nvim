ee8bf02a868b305979735f90e467d910fbc76cce
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-04-03 00:57:43 +0200

    Final touches

--- init.lua
+++ init.lua
@@ -248,7 +248,26 @@ require('lazy').setup({
   --    require('Comment').setup({})
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  {
+    'numToStr/Comment.nvim',
+    opts = {},
+    config = function()
+      require('Comment').setup {
+        toggler = {
+          ---Line-comment toggle keymap
+          line = '<C-c>',
+          ---Block-comment toggle keymap
+          block = 'gbc',
+        },
+        opleader = {
+          ---Line-comment keymap
+          line = '<C-c>',
+          ---Block-comment keymap
+          block = 'gb',
+        },
+      }
+    end,
+  },
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following lua:
@@ -266,9 +285,9 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
       on_attach = function(bufnr)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>hs', '<cmd>lua require"gitsigns".stage_hunk()<CR>', {})
         vim.api.nvim_buf_set_keymap(bufnr, 'n', ']g', '<cmd>lua require"gitsigns".next_hunk()<CR>', {})
         vim.api.nvim_buf_set_keymap(bufnr, 'n', '[g', '<cmd>lua require"gitsigns".prev_hunk()<CR>', {})
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gs', '<cmd>lua require"gitsigns".stage_hunk()<CR>', {})
         vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gr', '<cmd>lua require"gitsigns".reset_hunk()<CR>', {})
         vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gq', '<cmd>lua require"gitsigns".setloclist()<CR>', {})
         vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gh', '<cmd>lua require"gitsigns".preview_hunk()<CR>', {})
@@ -365,15 +384,115 @@ require('lazy').setup({
 
       -- [[ Configure Telescope ]]
       -- See `:help telescope` and `:help telescope.setup()`
+      local actions = require 'telescope.actions'
       require('telescope').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = {
+              ['<C-n>'] = actions.move_selection_next,
+              ['<C-p>'] = actions.move_selection_previous,
+
+              ['<C-c>'] = actions.close,
+
+              ['<Down>'] = actions.move_selection_next,
+              ['<Up>'] = actions.move_selection_previous,
+
+              ['<CR>'] = actions.select_default,
+              ['<C-x>'] = actions.select_horizontal,
+              ['<C-v>'] = actions.select_vertical,
+              ['<C-t>'] = actions.select_tab,
+
+              ['<C-u>'] = actions.preview_scrolling_up,
+              ['<C-d>'] = actions.preview_scrolling_down,
+              -- ['<C-f>'] = actions.preview_scrolling_left,
+              -- ['<C-k>'] = actions.preview_scrolling_right,
+
+              ['<PageUp>'] = actions.results_scrolling_up,
+              ['<PageDown>'] = actions.results_scrolling_down,
+              -- ['<M-f>'] = actions.results_scrolling_left,
+              -- ['<M-k>'] = actions.results_scrolling_right,
+
+              -- ['<Tab>'] = actions.toggle_selection + actions.move_selection_worse,
+              -- ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_better,
+              ['<A-CR>'] = actions.toggle_selection,
+              ['<Tab>'] = actions.move_selection_next,
+              ['<S-Tab>'] = actions.move_selection_previous,
+              ['<C-q>'] = actions.send_to_qflist + actions.open_qflist,
+              ['<M-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+              ['<C-l>'] = actions.complete_tag,
+              ['<C-/>'] = actions.which_key,
+              ['<C-_>'] = actions.which_key, -- keys from pressing <C-/>
+              ['<C-w>'] = { '<c-s-w>', type = 'command' },
+              -- ['<C-r><C-w>'] = actions.insert_original_cword,
+
+              -- disable c-j because we dont want to allow new lines #2123
+              ['<C-j>'] = actions.nop,
+            },
+            n = {
+              -- ['<LeftMouse>'] = {
+              --   actions.mouse_click,
+              --   type = 'action',
+              --   opts = { expr = true },
+              -- },
+              -- ['<2-LeftMouse>'] = {
+              --   actions.double_mouse_click,
+              --   type = 'action',
+              --   opts = { expr = true },
+              -- },
+
+              ['<esc>'] = actions.close,
+              ['<CR>'] = actions.select_default,
+              ['<C-x>'] = actions.select_horizontal,
+              ['<C-v>'] = actions.select_vertical,
+              ['<C-t>'] = actions.select_tab,
+
+              ['<A-CR>'] = actions.toggle_selection,
+              ['<Tab>'] = actions.move_selection_next,
+              ['<S-Tab>'] = actions.move_selection_previous,
+              -- ['<Tab>'] = actions.toggle_selection + actions.move_selection_worse,
+              -- ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_better,
+              ['<C-q>'] = actions.send_to_qflist + actions.open_qflist,
+              ['<M-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+
+              -- TODO: This would be weird if we switch the ordering.
+              ['j'] = actions.move_selection_next,
+              ['k'] = actions.move_selection_previous,
+              ['H'] = actions.move_to_top,
+              ['M'] = actions.move_to_middle,
+              ['L'] = actions.move_to_bottom,
+
+              ['<Down>'] = actions.move_selection_next,
+              ['<Up>'] = actions.move_selection_previous,
+              ['gg'] = actions.move_to_top,
+              ['G'] = actions.move_to_bottom,
+
+              ['<C-u>'] = actions.preview_scrolling_up,
+              ['<C-d>'] = actions.preview_scrolling_down,
+              -- ['<C-f>'] = actions.preview_scrolling_left,
+              -- ['<C-k>'] = actions.preview_scrolling_right,
+
+              ['<PageUp>'] = actions.results_scrolling_up,
+              ['<PageDown>'] = actions.results_scrolling_down,
+              -- ['<M-f>'] = actions.results_scrolling_left,
+              -- ['<M-k>'] = actions.results_scrolling_right,
+
+              ['?'] = actions.which_key,
+            },
+          },
+          layout_strategy = 'horizontal',
+          layout_config = {
+            horizontal = {
+              prompt_position = 'top',
+            },
+          },
+          sorting_strategy = 'ascending',
+          --   mappings = {
+          --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          --   },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
@@ -693,7 +812,8 @@ require('lazy').setup({
             luasnip.lsp_expand(args.body)
           end,
         },
-        completion = { completeopt = 'menu,menuone,noinsert' },
+        preselect = cmp.PreselectMode.None,
+        completion = { completeopt = 'menu,menuone,noselect' },
 
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
@@ -708,13 +828,13 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<CR>'] = cmp.mapping.confirm { select = true },
+          ['<CR>'] = cmp.mapping.confirm { select = false },
           ['<C-c>'] = cmp.mapping.abort(),
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
           --  completions whenever it has completion options available.
-          ['<C-Space>'] = cmp.mapping.complete {},
+          ['<A-CR>'] = cmp.mapping.complete {},
 
           -- Think of <c-l> as moving to the right of your snippet expansion.
           --  So if you have a snippet that's like:
@@ -795,17 +915,17 @@ require('lazy').setup({
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
+      -- local statusline = require 'mini.statusline'
       -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      -- statusline.setup { use_icons = vim.g.have_nerd_font }
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
+      -- statusline.section_location = function()
+      --   return '%2l:%-2v'
+      -- end
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
