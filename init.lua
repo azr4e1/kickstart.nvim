70a85b12a96f3327833c759955915d7e83e17699
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-04-08 12:21:04 +0100

    Added keybinding for todo-comment

--- init.lua
+++ init.lua
@@ -521,6 +521,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sj', builtin.jumplist, { desc = '[S]earch jumplist' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>tt', ':TodoTelescope<CR>', { desc = 'Search TODOs' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
