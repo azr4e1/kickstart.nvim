3248b812eafefeaae2443d312892b31be0197695
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-05-07 19:30:01 +0100

    feat(pylsp): configure to ignore import level warnings

--- init.lua
+++ init.lua
@@ -683,7 +683,15 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         gopls = {},
-        pylsp = {},
+        pylsp = {
+          settings = {
+            pylsp = {
+              plugins = {
+                autopep8 = { enabled = false },
+              },
+            },
+          },
+        },
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
